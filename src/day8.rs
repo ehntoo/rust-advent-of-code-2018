efa3b2a49def3f891e74143945694d863b738467
Mitchell Johnson <ehntoo@gmail.com>
2018-12-08 15:34:27 -0500

    Day 8 part 1. A good one, I think. :)

---
+++ src/day8.rs
@@ -0,0 +1,51 @@
+use std::boxed::Box;
+
+#[derive(PartialEq, Debug, Clone)]
+pub struct Node {
+    children: Vec<Node>,
+    metadata: Vec<u32>,
+}
+
+#[aoc_generator(day8)]
+pub fn input_generator(input: &str) -> Box<Node> {
+    let numbers: Vec<u32> = input.trim().split(" ").map(|s| s.parse().unwrap()).collect();
+    let mut iter = numbers.iter();
+    Box::new(node_from_number_iter(&mut iter))
+}
+
+fn node_from_number_iter<'a, I>(numbers: &mut I) -> Node 
+where I: Iterator<Item = &'a u32>,
+{
+    let num_children = numbers.next().unwrap();
+    let num_metadata = numbers.next().unwrap();
+    let children = (0 .. *num_children).map(|_| {
+        node_from_number_iter(numbers)
+    }).collect();
+    let metadata = numbers.take(*num_metadata as usize).map(|x| *x).collect();
+    Node { children, metadata }
+}
+
+#[aoc(day8, part1)]
+pub fn solve_part1(node: &Node) -> u32 {
+    node.metadata.iter().fold(0, |acc, x| acc + *x) + node.children.iter().fold(0, |acc, c| acc + solve_part1(c))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parser() {
+        assert_eq!(input_generator("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"),
+        Node {
+            children: vec![
+                Node { children: vec![], metadata: vec![10, 11, 12] },
+                Node {
+                    children: vec![ Node { children: vec![], metadata: vec![99] }],
+                    metadata: vec![2],
+                },
+            ],
+            metadata: vec![1, 1, 2],
+        });
+    }
+}
