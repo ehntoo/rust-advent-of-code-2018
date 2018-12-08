965e9686587ba13d7b91ed522bb4ba5e68709668
Mitchell Johnson <ehntoo@gmail.com>
2018-12-08 15:54:56 -0500

    Day 8 part two implemented. Recursion is fun.

--- src/day8.rs
+++ src/day8.rs
@@ -9,8 +9,7 @@ pub struct Node {
 #[aoc_generator(day8)]
 pub fn input_generator(input: &str) -> Box<Node> {
     let numbers: Vec<u32> = input.trim().split(" ").map(|s| s.parse().unwrap()).collect();
-    let mut iter = numbers.iter();
-    Box::new(node_from_number_iter(&mut iter))
+    Box::new(node_from_number_iter(&mut numbers.iter()))
 }
 
 fn node_from_number_iter<'a, I>(numbers: &mut I) -> Node 
@@ -27,7 +26,25 @@ where I: Iterator<Item = &'a u32>,
 
 #[aoc(day8, part1)]
 pub fn solve_part1(node: &Node) -> u32 {
-    node.metadata.iter().fold(0, |acc, x| acc + *x) + node.children.iter().fold(0, |acc, c| acc + solve_part1(c))
+    node.metadata.iter().fold(0, |acc, x| acc + *x) +
+        node.children.iter().fold(0, |acc, c| acc + solve_part1(c))
+}
+
+#[aoc(day8, part2)]
+pub fn solve_part2(node: &Node) -> u32 {
+    if node.children.len() == 0 {
+        node.metadata.iter().fold(0, |acc, x| acc + *x)
+    } else {
+        let child_sums: Vec<u32> = node.children.iter().map(|c| solve_part2(c)).collect();
+        node.metadata.iter().fold(0, |acc, x| {
+            let idx = (*x - 1) as usize;
+            acc + if idx < child_sums.len() {
+                child_sums[idx]
+            } else {
+                0
+            }
+        })
+    }
 }
 
 #[cfg(test)]
@@ -37,7 +54,7 @@ mod tests {
     #[test]
     fn test_parser() {
         assert_eq!(input_generator("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"),
-        Node {
+        Box::new(Node {
             children: vec![
                 Node { children: vec![], metadata: vec![10, 11, 12] },
                 Node {
@@ -46,6 +63,11 @@ mod tests {
                 },
             ],
             metadata: vec![1, 1, 2],
-        });
+        }));
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(solve_part2(&*input_generator("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2")), 66);
     }
 }
